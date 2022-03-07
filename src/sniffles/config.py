dddc558030c484964f065f064b06aa426afafa58
Moritz Smolka <moriqz@gmail.com>
2022-03-07 01:41:49 +0100

    Update version (Sniffles 2.0.3)

--- src/sniffles/config.py
+++ src/sniffles/config.py
@@ -16,7 +16,7 @@ import argparse
 from sniffles import util
 
 VERSION="Sniffles2"
-BUILD="2.0.2"
+BUILD="2.0.3"
 SNF_VERSION="S2_rc3"
 
 class ArgFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter):
