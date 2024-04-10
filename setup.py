8347167c776b51b8c398cf97b1b1bfecffb82c03
Luis Paulin <lfpaulin@gmail.com>
2024-04-10 07:27:22 -0600

    setup and release for pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sniffles',
+    version='2.3',
+    packages=find_packages(),
+    url='https://github.com/fritzsedlazeck/Sniffles',
+    license='MIT',
+    author='Moritz Smolka, Hermann Romanek',
+    author_email='moritz.g.smolka@gmail.com, sniffles@romanek.at',
+    description='A fast structural variation caller for long-read sequencing data',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
